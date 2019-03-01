d9d9eb03ffed9003d1ce78ae59e75e2a738f9f8d
josh <josh@joshuadavidthomas.com>
2019-02-28 19:34:53 -0600

    create symlinks from dotfiles to home folder

---
+++ symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dir=~/.dotfiles
+olddir=~/.dotfiles_old
+files="bashrc zshrc"
+
+# create $olddir in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in home directory."
+    ln -sf $dir/.$file ~/.$file 
+done
