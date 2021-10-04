2c218dcf79586bd52b7a6766917fcff803fff796
Josh <josh@joshthomas.dev>
2021-10-04 15:21:11 -0500

    fix line endings

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ INSTALL_DIR=${HOME}/.dotfiles
 
 git clone git@github.com:joshuadavidthomas/dotfiles.git ${INSTALL_DIR}
 
-ln -sf ${INSTALL_DIR}/.aliases ${HOME}/.aliases 
-ln -sf ${INSTALL_DIR}/.gitconfig ${HOME}/.gitconfig 
-ln -sf ${INSTALL_DIR}/.npmrc ${HOME}/.npmrc 
-ln -sf ${INSTALL_DIR}/.zprofile ${HOME}/.zprofile 
-ln -sf ${INSTALL_DIR}/.zshenv ${HOME}/.zshenv 
-ln -sf ${INSTALL_DIR}/.zshrc ${HOME}/.zshrc 
+ln -sf ${INSTALL_DIR}/.aliases ${HOME}/.aliases
+ln -sf ${INSTALL_DIR}/.gitconfig ${HOME}/.gitconfig
+ln -sf ${INSTALL_DIR}/.npmrc ${HOME}/.npmrc
+ln -sf ${INSTALL_DIR}/.zprofile ${HOME}/.zprofile
+ln -sf ${INSTALL_DIR}/.zshenv ${HOME}/.zshenv
+ln -sf ${INSTALL_DIR}/.zshrc ${HOME}/.zshrc
 
 for binary in ${INSTALL_DIR}/bin/*
 do
