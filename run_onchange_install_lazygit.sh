cb1b4dcb4ed40bd7960abf9d7b738981a7cc541d
Josh <josh@joshthomas.dev>
2023-06-07 13:18:17 -0500

    add lazygit installation script

---
+++ run_onchange_install_lazygit.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+sudo rm -f /usr/local/bin/lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf /tmp/lazygit.tar.gz -C /tmp
+sudo install -m755 /tmp/lazygit /usr/local/bin/lazygit
