01bcfda40e6256bd3cb53e47b3f6f311ac41629c
Josh <josh@joshthomas.dev>
2023-12-01 13:38:35 -0600

    add gh and bun completions & setup zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,9 @@ starship init fish | source
 # direnv
 direnv hook fish | source
 
+# zoxide
+zoxide init fish | source
+
 # pyenv
 pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
