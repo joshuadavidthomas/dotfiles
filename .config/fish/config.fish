177a270b0cabd40ef2f26e1df0f49463a7fd780a
Josh <josh@joshthomas.dev>
2023-12-01 12:17:22 -0600

    switch to fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# starship
+starship init fish | source
+
+# direnv
+direnv hook fish | source
+
+# pyenv
+pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
