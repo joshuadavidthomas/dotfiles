028ddf08599cd04abfd1615df75f8b55d2babb1b
Josh <josh@joshthomas.dev>
2024-05-23 11:46:27 -0500

    tabs to spaces, fight me

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,59 +3,59 @@ local act = wezterm.action
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 wezterm.on("augment-command-palette", function(_, _)
-	return {
-		{
-			brief = "Rename tab",
-			icon = "md_rename_box",
+  return {
+    {
+      brief = "Rename tab",
+      icon = "md_rename_box",
 
-			action = act.PromptInputLine({
-				description = "Enter new name for tab",
-				action = wezterm.action_callback(function(window, _, line)
-					if line then
-						window:active_tab():set_title(line)
-					end
-				end),
-			}),
-		},
-	}
+      action = act.PromptInputLine({
+        description = "Enter new name for tab",
+        action = wezterm.action_callback(function(window, _, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      }),
+    },
+  }
 end)
 
 wezterm.on("user-var-changed", function(window, pane, name, value)
-	wezterm.log_info("var", name, value)
+  wezterm.log_info("var", name, value)
 end)
 
 wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
+  local overrides = window:get_config_overrides() or {}
+  if name == "ZEN_MODE" then
+    local incremental = value:find("+")
+    local number_value = tonumber(value)
+    if incremental ~= nil then
+      while number_value > 0 do
+        window:perform_action(wezterm.action.IncreaseFontSize, pane)
+        number_value = number_value - 1
+      end
+      overrides.enable_tab_bar = false
+    elseif number_value < 0 then
+      window:perform_action(wezterm.action.ResetFontSize, pane)
+      overrides.font_size = nil
+      overrides.enable_tab_bar = true
+    else
+      overrides.font_size = number_value
+      overrides.enable_tab_bar = false
+    end
+  end
+  window:set_config_overrides(overrides)
 end)
 
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 config.front_end = "WebGpu"
@@ -72,21 +72,21 @@ config.color_scheme = "tokyonight_storm"
 wezterm.add_to_config_reload_watch_list(config.color_scheme_dirs[1] .. config.color_scheme .. ".toml")
 
 if wezterm.target_triple:find("windows") then
-	config.default_domain = "WSL:Ubuntu"
-	config.window_decorations = "TITLE | RESIZE"
-	config.window_frame = {
-		active_titlebar_bg = "black",
-	}
+  config.default_domain = "WSL:Ubuntu"
+  config.window_decorations = "TITLE | RESIZE"
+  config.window_frame = {
+    active_titlebar_bg = "black",
+  }
 else
-	config.default_domain = "local"
-	config.window_decorations = "TITLE | RESIZE"
+  config.default_domain = "local"
+  config.window_decorations = "TITLE | RESIZE"
 end
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 config.font = wezterm.font({ family = "MonoLisa Variable" })
