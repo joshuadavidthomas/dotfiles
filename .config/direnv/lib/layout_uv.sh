adb96be6c1ea77dd1ff00067d226cd7aec5eeaa7
Josh <josh@joshthomas.dev>
2024-10-10 09:07:20 -0500

    switch to uv sync for uv layout

--- .config/direnv/lib/layout_uv.sh
+++ .config/direnv/lib/layout_uv.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
+set -oue pipefail
+
 layout_uv() {
-  if [[ -d ".venv" ]]; then
-    VIRTUAL_ENV="$(pwd)/.venv"
-  fi
+        VIRTUAL_ENV="$(pwd)/.venv"
 
-  if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
-    log_status "No virtual environment exists. Executing \`uv venv\` to create one."
-    if [[ -f ".mise*.toml" ]]; then
-      log_status "mise detected, using it's Python version"
-      uv venv --python-preference installed --seed
-    else
-      uv venv --seed
-    fi
-    VIRTUAL_ENV="$(pwd)/.venv"
-  fi
+        if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+                log_status "No virtual environment exists. Executing \`uv venv\` to create one."
+                uv venv
+                VIRTUAL_ENV="$(pwd)/.venv"
+        fi
 
-  PATH_add "$VIRTUAL_ENV/bin"
-  export UV_ACTIVE=1
-  export VIRTUAL_ENV
+        if [[ -f "uv.lock" ]]; then
+                uv sync
+        fi
+
+        PATH_add "$VIRTUAL_ENV/bin"
+        export UV_ACTIVE=1 # or VENV_ACTIVE=1
+        export VIRTUAL_ENV
 }
