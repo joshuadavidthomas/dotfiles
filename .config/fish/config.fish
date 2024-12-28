947524f985b26caa7cfa49fb00635f20db0ee6c6
Josh <josh@joshthomas.dev>
2024-12-27 21:24:06 -0600

    remove tmux fallback

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,3 @@
-if status is-interactive
-    # Check if we are not already in a tmux session and if the terminal is not WezTerm
-    if not set -q TMUX; and not string match -q WezTerm $TERM_PROGRAM
-        exec tmux # Start tmux
-    end
-    # If the terminal is WezTerm or already in a tmux session, continue with normal fish initialization
-end
-
 # disable intro fish greeting
 set -g fish_greeting
 set -gx EDITOR vim
